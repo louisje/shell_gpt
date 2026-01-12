fec53e2be48c8e83095c2336e67e1b804c6d9501
Louis Jeng <louisje@gmail.com>
2026-01-12 12:00:06 +0800

    將 shell 集成快捷鍵從 Ctrl-B 改為 Ctrl-S，並添加交互式終端檢查

--- sgpt/integration.py
+++ sgpt/integration.py
@@ -1,27 +1,33 @@
 bash_integration = """
 # Shell-GPT integration BASH v0.2
-_sgpt_bash() {
-    if [[ -n "$READLINE_LINE" ]]; then
-        READLINE_LINE=$(sgpt --shell <<< "$READLINE_LINE" --no-interaction)
-        READLINE_POINT=${#READLINE_LINE}
-    fi
+[[ $- == *i* ]] && [[ -t 0 ]] && {
+    stty -ixon  # Disable flow control to allow Ctrl-S
+    _sgpt_bash() {
+        if [[ -n "$READLINE_LINE" ]]; then
+            READLINE_LINE=$(sgpt --shell <<< "$READLINE_LINE" --no-interaction)
+            READLINE_POINT=${#READLINE_LINE}
+        fi
+    }
+    bind -x '"\\C-s": _sgpt_bash'
 }
-bind -x '"\\C-b": _sgpt_bash'
 # Shell-GPT integration BASH v0.2
 """
 
 zsh_integration = """
 # Shell-GPT integration ZSH v0.2
-_sgpt_zsh() {
-    if [[ -n "$BUFFER" ]]; then
-        _sgpt_prev_cmd=$BUFFER
-        BUFFER+="⌛"
-        zle -I && zle redisplay
-        BUFFER=$(sgpt --shell <<< "$_sgpt_prev_cmd" --no-interaction)
-        zle end-of-line
-    fi
+[[ $- == *i* ]] && [[ -t 0 ]] && {
+    stty -ixon  # Disable flow control to allow Ctrl-S
+    _sgpt_zsh() {
+        if [[ -n "$BUFFER" ]]; then
+            _sgpt_prev_cmd=$BUFFER
+            BUFFER+="⌛"
+            zle -I && zle redisplay
+            BUFFER=$(sgpt --shell <<< "$_sgpt_prev_cmd" --no-interaction)
+            zle end-of-line
+        fi
+    }
+    zle -N _sgpt_zsh
+    bindkey ^s _sgpt_zsh
 }
-zle -N _sgpt_zsh
-bindkey ^b _sgpt_zsh
 # Shell-GPT integration ZSH v0.2
 """
